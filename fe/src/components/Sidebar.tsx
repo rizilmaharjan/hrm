70888376417390a0b6baa06b180ce7299bd87792
rizilmaharjan <rizilmaharjan@gmail.com>
2024-04-22 10:35:41 +0545

    Refactor: Remove logout from sidebar

--- fe/src/components/Sidebar.tsx
+++ fe/src/components/Sidebar.tsx
@@ -1,27 +1,7 @@
-import { Instance } from "../utils/Instance";
-import { NavLink, useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
-import { useAppDispatch } from "../redux/hooks";
-import { logout } from "../redux/user/userSlice";
-import { LogoutIcon } from "../assets/svg";
+import { NavLink } from "react-router-dom";
 import { sidebarItems } from "../constants";
 
 export default function Sidebar() {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-
-  const handleLogout = async () => {
-    try {
-      const res = await Instance.get("/v1/auth/logout");
-      dispatch(logout());
-      // localStorage.removeItem("username");
-      navigate("/");
-      toast.success(res.data.message);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
       <aside
@@ -51,83 +31,6 @@ export default function Sidebar() {
                 </NavLink>
               </li>
             ))}
-            {/* <NavLink
-              to="/dashboard"
-              className={({ isActive }) =>
-                isActive
-                  ? "font-bold p-2 rounded-lg bg-gray-700 text-white hover:bg-gray-700"
-                  : ""
-              }
-            >
-              <li className="flex items-center">
-                <HomeIcon />
-                <span className="ms-3 hidden sm:block">Dashboard</span>
-              </li>
-            </NavLink>
-            <li className="p-2 rounded-lg text-white hover:bg-gray-700">
-              <NavLink
-                className={({ isActive }) =>
-                  isActive
-                    ? "flex item-center font-bold bg-gray-700 rounded-lg"
-                    : "flex item-center font-bold"
-                }
-                to="/employee"
-              >
-                <EmployeeIcon />
-                <span className="flex-1 ms-3 whitespace-nowrap hidden sm:block">
-                  Employee
-                </span>
-              </NavLink>
-            </li>
-            <li className=" p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <NavLink className="flex items-center" to="/payroll">
-                <DollarSignIcon />
-                <span className="flex-1 ms-3 whitespace-nowrap hidden sm:block">
-                  Payroll
-                </span>
-              </NavLink>
-            </li>
-
-            <li className=" p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <NavLink className="flex items-center" to="/attendence">
-                <ClockIcon />
-                <span className="flex-1 ms-3 whitespace-nowrap hidden sm:block">
-                  Attendence
-                </span>
-              </NavLink>
-            </li>
-            <li className=" p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <NavLink className="flex items-center" to="/service-events">
-                <ServiceEventIcon />
-                <span className="ms-3 hidden sm:block">Services</span>
-              </NavLink>
-            </li>
-            <li className=" p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <NavLink className="flex items-center" to="/allowance">
-                <AllowanceIcon />
-
-                <span className="ms-3">Allowance</span>
-              </NavLink>
-            </li>
-
-            <li className=" p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <NavLink className="flex items-center" to="/job-type">
-                <JobTypeIcon />
-                <span className="flex-1 ms-3 whitespace-nowrap hidden sm:block">
-                  Job Type
-                </span>
-              </NavLink>
-            </li> */}
-
-            <li
-              className="flex items-center p-2 rounded-lg cursor-pointer text-white hover:bg-gray-700 group"
-              onClick={handleLogout}
-            >
-              <LogoutIcon className="h-5 w-5" />
-              <span className="flex-1 ms-3 whitespace-nowrap hidden sm:block">
-                Log Out
-              </span>
-            </li>
           </ul>
         </div>
       </aside>
